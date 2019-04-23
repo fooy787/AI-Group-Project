61e2ff608e80ff42459ab31e135e8f4786d0cd84
LilSquirtle <jackieha317@gmail.com>
2019-04-23 13:44:14 -0400

    AHHHHHH
    Professor and StudentPop should be back

---
+++ AIGroupProject/AIGroupProject/Professor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIGroupProject
+{
+    class Professor
+    {
+        private int ID;
+        private string Name;
+        private List<Courses> Classes;
+
+        public Professor(string name, int id)
+        {
+            Name = name;
+            ID = id;
+            Classes = new List<Courses>();
+        }
+
+        public void AddCourse(Courses course)
+        {
+            Classes.Add(course);
+        }
+
+        public List<Courses> GetCourses()
+        {
+            return Classes;
+        }
+        
+    }
+}
---
+++ AIGroupProject/AIGroupProject/StudentPop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIGroupProject
+{
+    class StudentPop
+    {
+        //our number of students in the population
+        private int numStudents;
+
+        //List of classes that the population attends
+        private List<string> classes;
+
+
+        public StudentPop(int numberOfStudents)
+        {
+
+        }
+
+        public void AddCLass(string className)
+        {
+
+        }
+
+        public int getNumStudents()
+        {
+            return numStudents;
+        }
+
+        public List<string> getClasses()
+        {
+            return classes;
+        }   
+    }
+}
