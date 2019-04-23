27ded1ed705ee6aab3379196aaccfc0c763899b4
Chase Williams <fooy787@gmail.com>
2019-04-23 13:33:11 -0400

    Professors

---
+++ AIGroupProject/AIGroupProject/Professor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIGroupProject
+{
+    class Professor
+    {
+        private int ID;
+        private string Name;
+        private List<Course> Classes;
+
+        public Professor(string name, int id)
+        {
+            Name = name;
+            ID = id;
+            Classes = new List<Course>();
+        }
+
+        public void AddCourse(Course course)
+        {
+            Classes.Add(course);
+        }
+
+        public List<Course> GetCourses()
+        {
+            return Classes;
+        }
+    }
+}
