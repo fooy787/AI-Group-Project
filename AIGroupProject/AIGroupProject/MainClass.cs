49c4ebfec7f1e5156b00251c1b4e4ceef7c91f05
LilSquirtle <jackieha317@gmail.com>
2019-04-26 20:15:23 -0400

    courselist is can be parsed (I think)
    didn't do anything with duration though

--- AIGroupProject/AIGroupProject/MainClass.cs
+++ AIGroupProject/AIGroupProject/MainClass.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +11,51 @@ namespace AIGroupProject
     {
         static void Main(string[] args)
         {
+            HashSet<Professor> profs = new HashSet<Professor>();
+            HashSet<Course> courses = new HashSet<Course>();
+            string file;
+            if (args.Length == 0)
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "All stuff|*.*";
+                dlg.ShowDialog();
+                file = dlg.FileName;
+                if (file.Trim().Length == 0)
+                {
+                    return;
+                }
+                dlg.Dispose();
+            }
+            else { file = args[0]; }
+
+            string toRead = File.ReadAllText(file);
+            string line = "";
+            int lineNum = 0;
+            int i = 0;
+
+            while (i < toRead.Length)
+            {
+                if (toRead[i] != '\n')
+                    line += toRead[i];
+                else
+                {
+                    if (line.Length != 0)
+                    {
+                        List<string> info = line.Split(',').ToList();
+                        if (lineNum != 0)
+                        {
+                            Course c = new Course(info[0], Int32.Parse(info[1]));
+                            Professor p = new Professor(info[2], Int32.Parse(info[3]));
+                            courses.Add(c);
+                            profs.Add(p);
+                        }
+                        line = "";
+                    }
+                    lineNum++;
+                }
+                i++;
+            }
+
         }
     }
 }
